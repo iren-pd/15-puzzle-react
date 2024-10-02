4776e9fd72638f28271e691d820a2cd43db30084
iren-pd <irenpd45@gmail.com>
2024-10-02 16:25:48 +0300

    Added counter functionality

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,28 @@
-import NxWelcome from './nx-welcome';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from '../redux/slices/App';
+import type { RootState } from '../redux/store';
 
 export function App() {
+  const count = useSelector((state: RootState) => state.app.value);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <NxWelcome title="iren" />
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 import App from './app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
