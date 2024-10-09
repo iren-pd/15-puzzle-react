5a65ea11ba211edb30ad551e21ae5d73654ba3cb
iren-pd <irenpd45@gmail.com>
2024-10-09 14:21:09 +0300

    Edit step animation

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,5 +1,3 @@
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
 import { useRef, useState } from 'react';
 import cellBackground from '../../img/icon-board.svg';
 
@@ -16,42 +14,43 @@ export const Cell = ({
 }) => {
   const styleForCell =
     'px-2 text-3xl flex items-center justify-center text-pink-700 font-bold absolute inset-0';
+  const backgroundImage = {
+    width: `${cellSize}px`,
+    height: `${cellSize}px`,
+    backgroundImage: `url(${cellBackground})`,
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  };
   const [isHovered, setIsHovered] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
 
-//   useGSAP((context, contextSafe) => {
-//     const onClickCell = () => {
-//       if (contextSafe) {
-//         contextSafe(() => {
-//           if (ref.current) {
-//             gsap.to(ref.current, { x: `+=${cellSize}` });
-//           }
-//         })();
-//       }
-//     };
+  //   useGSAP((context, contextSafe) => {
+  //     const onClickCell = () => {
+  //       if (contextSafe) {
+  //         contextSafe(() => {
+  //           if (ref.current) {
+  //             gsap.to(ref.current, { x: `+=${cellSize}` });
+  //           }
+  //         })();
+  //       }
+  //     };
 
-//     if (ref.current) {
-//       ref.current.addEventListener('click', onClickCell);
-//     }
+  //     if (ref.current) {
+  //       ref.current.addEventListener('click', onClickCell);
+  //     }
 
-//     return () => {
-//       if (ref.current) {
-//         ref.current.removeEventListener('click', onClickCell);
-//       }
-//     };
-//   }, {});
+  //     return () => {
+  //       if (ref.current) {
+  //         ref.current.removeEventListener('click', onClickCell);
+  //       }
+  //     };
+  //   }, {});
 
   return (
     <div
       className={`relative border border-pink-500`}
-      style={{
-        width: `${cellSize}px`,
-        height: `${cellSize}px`,
-        backgroundImage: `url(${cellBackground})`,
-        backgroundSize: 'contain',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-      }}
+      style={backgroundImage}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       onClick={onClick}
@@ -62,6 +61,7 @@ export const Cell = ({
           isHovered && isNear ? ' cursor-pointer' : ''
         } `}
         style={{
+          backgroundImage,
           backgroundColor:
             isHovered && isNear ? 'rgba(255, 182, 193, 0.5)' : 'transparent',
         }}
